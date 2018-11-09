b139927836e3fd399a74fd6c8822d19cef7f8624
Chris <chris@uber5.com>
2018-11-09 18:15:40 +0200

    Refactor a little, added README.md

---
+++ README.md
@@ -0,0 +1,40 @@
+# Opinionated Logging for NodeJS
+
+Setting up logging that is useful and flexible, for both development and production, seems hard.
+
+This is an opinionated approach, with sane defaults, for servers (HTTP or otherwise) written in NodeJS.
+
+# Principles and Settings
+
+- Use `console.log` as you wish in development, but (by default) suppress its output in production (`NODE_ENV === 'production'`).
+- In order to see `console.log` output in production, set `WINSTON_LEVEL` to `debug` (which will also enable all [winston]() based `logger.debug()` logs).
+- log output in production is into `./app-%date%.log`, no output on the console itself.
+- Log output, when not in production, is to the console, nothing written to files.
+- In production, an additional file `error-%date%.log` is written with actual errors.
+- Files written are limited in size and rotated.
+- Files older than 14 days are removed.
+
+# Logging via Winston
+
+```
+const { logger } = require('u5-nodejs-logging')
+
+...
+logger.info('successfully saved')
+...
+```
+
+# Request Logging and Error Handling for ExpressJS
+
+```
+const {
+  expressLoggingMiddleware, expressErrorHandlingMiddleware
+} = require('u5-nodejs-logging')
+
+const app = express()
+
+app.use(expressLoggingMiddleware()) // first middleware
+// ... routes etc here, via app.get() etc.
+app.use(expressErrorHandlingMiddleware()) // last middleware
+app.listen(process.env.PORT || 3000)
+```
--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ const util = require('util')
 const morgan = require('morgan')
 const winston = require('winston')
 
+require('winston-daily-rotate-file')
+
 const errorStackFormat = winston.format(info => {
   if (info instanceof Error) {
     return Object.assign({}, info, {
@@ -15,29 +17,36 @@ const errorStackFormat = winston.format(info => {
 // TODO: depending on environment, we could define other transports, e.g.
 // logging to ELK or elsewhere.
 
+const transports = process.env.NODE_ENV === 'production' ? [
+  // production
+  new (winston.transports.DailyRotateFile)({
+    filename: 'error-%DATE%.log',
+    // datePattern: 'YYYY-MM-DD-HH',
+    // zippedArchive: true,
+    maxSize: '20m',
+    maxFiles: '14d',
+    level: 'error'
+  }),
+  new (winston.transports.DailyRotateFile)({
+    filename: 'app-%DATE%.log',
+    // datePattern: 'YYYY-MM-DD-HH',
+    // zippedArchive: true,
+    maxSize: '20m',
+    maxFiles: '14d'
+  })
+] : [
+  // not production
+  new winston.transports.Console({
+    format: winston.format.simple()
+  })
+]
+
 const logger = winston.createLogger({
   level: process.env.WINSTON_LEVEL || 'info',
   format: winston.format.combine(errorStackFormat(), winston.format.json()),
-  transports: [
-    //
-    // - Write to all logs with level `info` and below to `combined.log` 
-    // - Write all logs error (and below) to `error.log`.
-    //
-    new winston.transports.File({ filename: 'error.log', level: 'error' }),
-    new winston.transports.File({ filename: 'combined.log' })
-  ]
+  transports
 });
  
-//
-// If we're not in production then log to the `console` with the format:
-// `${info.level}: ${info.message} JSON.stringify({ ...rest }) `
-// 
-if (process.env.NODE_ENV !== 'production') {
-  logger.add(new winston.transports.Console({
-    format: winston.format.simple()
-  }));
-}
-
 const originalLog = console.log
 console.log = function() {
   if (process.env.NODE_ENV === 'production') {
